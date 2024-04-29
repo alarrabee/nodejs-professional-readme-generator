71dd0e903c9499e09e882e4a13a93e77791a8788
alarrabee <alexlarrabee10@gmail.com>
2024-04-28 22:16:16 -0500

    added an array of questions for user input

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "Project Title:",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Project description:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Installation instructions:",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Usage information:",
+        name: "usage",
+    },
+    {
+        type: "list",
+        message: "Select a license:",
+        name: "license",
+        choices: ["Apache", "Eclipse", "IBM", "MIT", "Mozilla", "Zlib"]
+    },
+    {
+        type: "input",
+        message: "Contribution guidelines:",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "Test instructions:",
+        name: "tests",
+    },
+    {
+        type: "input",
+        message: "Enter your GitHub username:",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "Enter your email address",
+        name: "email",
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
